8b0cd41599a4d054f0df7597608d0d3473515303
chandannir <chandannir82@gmail.com>
2025-05-04 23:38:21 -0400

    updated akin'

--- mis/akin.py
+++ mis/akin.py
@@ -1,35 +1,33 @@
+from akinator.async_aki import Akinator
 import asyncio
-from akinator_async import Akinator # type: ignore
+
+aki = Akinator()
 
 async def main():
-    aki = Akinator()
-    try:
-        question = await aki.start_game(language='en', child_mode=False, region='en')
-        while aki.progression <= 80:
-            print(question)
-            answer = input("Answer (yes/no/idk/probably/probably not): ").lower()
+    print("Welcome to Akinator!")
+    await aki.start_game(language="en")
 
-            if answer == "yes":
-                question = await aki.answer("y")
-            elif answer == "no":
-                question = await aki.answer("n")
-            elif answer in ["idk", "don't know"]:
-                question = await aki.answer("idk")
-            elif answer == "probably":
-                question = await aki.answer("p")
-            elif answer == "probably not":
-                question = await aki.answer("pn")
-            else:
-                print("Invalid answer.")
+    while aki.progression <= 80:
+        print(aki.question)
+        answer = input("Your answer (yes/no/idk/probably/probably not): ").lower()
 
-        await aki.win()
-        print(f"Is it {aki.first_guess['name']} ({aki.first_guess['description']})?")
-        final = input("yes/no: ").lower()
-        if final == "yes":
-            print("I guessed it!")
-        else:
-            print("I failed to guess it.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        try:
+            if answer not in ['yes', 'no', 'idk', 'probably', 'probably not']:
+                print("Invalid answer. Please try again.")
+                continue
+
+            await aki.answer(answer)
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+
+    await aki.win()
+    print(f"\nI guess: {aki.first_guess['name']} ({aki.first_guess['description']})")
+    print(f"Image: {aki.first_guess['absolute_picture_path']}")
+    correct = input("Was I correct? (yes/no): ").lower()
+    if correct == "yes":
+        print("Yay! I guessed it right 😎")
+    else:
+        print("Oh no! Maybe next time 😢")
 
 asyncio.run(main())
