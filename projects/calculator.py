5b77e681e1da3a63c803d1dbff73c7d09740b4aa
chandannir <chandannir82@gmail.com>
2025-05-10 21:47:52 -0400

    calculator GUI using tkinter

---
+++ projects/calculator.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evalyate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+
+root = tk.Tk()
+root.geometry("300x275")
+
+root.mainloop()
