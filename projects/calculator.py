8bbb2505e91fb843dbdd458cae0c4df33dc888a6
chandannir <chandannir82@gmail.com>
2025-05-12 21:36:51 -0400

    misc.

--- projects/calculator.py
+++ projects/calculator.py
@@ -3,16 +3,36 @@ import tkinter as tk
 calculation = ""
 
 def add_to_calculation(symbol):
-    pass
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0,"end")
+    text_result.insert(1.0,calculation)
 
-def evalyate_calculation():
-    pass
+def evaluate_calculation():
+    global calculation
+    try:
+        result = str(eval(calculation))
+        calculation = ""
+        text_result.delete(1.0,"end")
+        text_result.insert(1.0, calculation)
+    except: 
+        clear_field()
+        text_result.insert(1.0, "Error")
+        
+    
 
 def clear_field():
+    global calculation
+    calculation = ""
+    text_result.delete = (1.0,"end")
     pass
 
 
 root = tk.Tk()
 root.geometry("300x275")
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial",24))
+text_result.grid(columnspan=5)
+
 
 root.mainloop()
