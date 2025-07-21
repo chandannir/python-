f8d74bb4e0341977c21fae1f652af8fad7c85e28
chandannir <chandannir82@gmail.com>
2025-07-21 14:17:48 -0400

    classes and decoraters

---
+++ projects/web-dev-projects/cs50/dictionary.py
@@ -0,0 +1,9 @@
+houses ={"Harry": "Gryfiindor"}
+
+print(houses["Harry"])
+
+def sqaure(x):
+    return x*x
+
+for i in range(10):
+    print(sqaure(i))
--- projects/web-dev-projects/cs50/name.py
+++ projects/web-dev-projects/cs50/name.py
@@ -1,2 +1,22 @@
-name = input("Name: ")
-print(f"Hello, {name}")
+# name = input("Name: ")
+# print(f"Hello, {name}")
+
+# n = input("Number: ")
+
+# if n > 0:
+    #print("n is postive")
+    
+    
+names = ["harry", "ron", "Chandan", "Nir"]
+print(names[0])
+
+names.append("Draco")
+names.sort()
+
+s = set()
+
+s.add(1)
+s.add(3)
+  
+for name in names:
+    print(name)
