e3e5760f8e0ecd80bd446c40e962a5ae3fbcde2b
chandannir <chandannir82@gmail.com>
2025-07-27 13:46:30 -0400

    fuel gauge program

---
+++ projects/cs50/pset3/fuel.py
@@ -0,0 +1,27 @@
+
+def fuel_gauge():
+    while True:
+        fraction = input("Fraction: ").split("/")
+        try:
+            if len(fraction) == 2:
+                numerator = int(fraction[0])
+                denominator = int(fraction[1])
+            else:
+                continue
+            
+            fuel_amount = (numerator / denominator) * 100
+            if fuel_amount <= 1:
+                print("E")
+            elif fuel_amount >= 99:
+                print("F") 
+            else:
+                print(f"{round(fuel_amount)}")
+        
+        except ZeroDivisionError:
+            print("You cannot divide by zero!")
+            continue
+        except ValueError:
+            print("Unable to Divide!")
+            continue
+            
+fuel_gauge()
