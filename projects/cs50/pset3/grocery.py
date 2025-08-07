b8c3926812cf5a4b37a730de5bef97954ec6852a
chandannir <chandannir82@gmail.com>
2025-08-07 17:29:40 -0400

    grocery

---
+++ projects/cs50/pset3/grocery.py
@@ -0,0 +1,15 @@
+items = {}
+i = 0
+
+while True:
+    try:    
+        grocery = input("").upper()
+        if grocery not in items.values():
+            items[i] = grocery
+            i += 1
+        else:
+            continue
+    except EOFError:
+        for i, groceries in items.items():
+            print(i, groceries)
+        break
